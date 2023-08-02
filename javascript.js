4b44485f7ef26ebc28de2ae415098296eb7aedb5
TGDavid69 <79339729+TGDavid69@users.noreply.github.com>
2023-08-02 13:48:05 -0700

    Create a function to play a single round
    The function plays a round of rock, paper, and scissors while
    returning a string describing the round and adjusting the winner's
    score.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 
+//unsure how to solve the issue of keeping score
+//without using global variables like this. I'm assuming
+//we ought to use some form of set/get functions to
+//keep the variables within our functions but yeah.
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
 
-    let computerChoice = Math.floor(Math.random() * 3);
+    const computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice){
         case 0:
             return "Rock";
@@ -14,3 +21,73 @@ function getComputerChoice(){
             break;
     }
 }
+
+//removed the parameters because it was driving me insane
+//going back and forth when testing to see if the functions worked
+//I don't even think they are necessary, seeing as one is a user prompt
+//and the other is randomly generated. I don't see the need for parameters here
+function singleRound(){
+
+    let userInput = prompt("What would you like to throw: ");
+    let playerSelection = userInput.toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    const tieMessage = `It's a tie! Both you and the computer chose ${computerSelection}`;
+
+    let roundResult;
+    switch (playerSelection){
+        case "rock":
+            if (computerSelection === "Rock"){
+                roundResult = tieMessage;
+            }
+            else if (computerSelection === "Paper"){
+                roundResult = "You lose! Rock loses to Paper.";
+                computerScore++;
+            }
+            else if (computerSelection === "Scissors"){
+                roundResult = "You win! Rock beats Scissors.";
+                playerScore++;
+            }
+            break;
+
+        case "paper":
+            if (computerSelection === "Rock"){
+                roundResult = "You win! Paper beats Rock.";
+                playerScore++;
+            }
+            else if (computerSelection === "Paper"){
+                roundResult = tieMessage;
+            }
+            else if (computerSelection === "Scissors"){
+                roundResult = "You lose! Paper loses to Scissors.";
+                computerScore++;
+            }
+            break;
+
+        case "scissors":
+            if (computerSelection === "Rock"){
+                roundResult = "You lose! Scissors loses to Rock.";
+                computerScore++;
+            }
+            else if (computerSelection === "Paper"){
+                roundResult = "You win! Scissors beats Paper.";
+                playerScore++;
+            }
+            else if (computerSelection === "Scissors"){
+                roundResult = tieMessage;
+            }
+            break;
+
+        default: 
+            roundResult = "Please only enter either rock, paper, or scissors. Round was not played.";
+    }
+    return roundResult;    
+}
+
+
+
+/* console.log(singleRound());
+console.log(singleRound());
+console.log(singleRound());
+console.log(playerScore);
+console.log(computerScore); */
